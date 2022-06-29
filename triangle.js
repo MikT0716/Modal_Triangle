ce3ffdf91d1d49396338a615f9b9474606e4b8fc
MikT0716 <tothmiklos0716@gmail.com>
2022-06-29 22:10:29 +0200

    first commit

---
+++ triangle.js
@@ -0,0 +1,43 @@
+var reset = document.getElementById("reset");
+
+document.getElementById("calc").addEventListener("click", function () {
+  var a = parseInt(document.getElementById("a").value);
+  var b = parseInt(document.getElementById("b").value);
+  var c = parseInt(document.getElementById("c").value);
+  var s = (a + b + c) / 2;
+  var area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+  console.log(a);
+  if (a + b > c && a + c > b && b + c > a) {
+    document.getElementById("area").innerHTML =
+      "<b>" + area.toFixed(3) + "</b>";
+    reset.style.visibility = "visible";
+  } else {
+    document.getElementById("calc").innerHTML =
+      "A &#9651 nem szerkeszthető meg";
+    document.getElementById("area").innerHTML = "";
+    document.getElementById("calc").style.cursor = "not-allowed";
+    document.getElementById("a").style.cursor = "not-allowed";
+    document.getElementById("a").disabled = true;
+    document.getElementById("b").style.cursor = "not-allowed";
+    document.getElementById("b").disabled = true;
+    document.getElementById("c").style.cursor = "not-allowed";
+    document.getElementById("c").disabled = true;
+    reset.style.visibility = "visible";
+  }
+});
+
+reset.addEventListener("click", function () {
+  document.getElementById("a").value = "";
+  document.getElementById("b").value = "";
+  document.getElementById("c").value = "";
+  document.getElementById("calc").style.cursor = "auto";
+  document.getElementById("calc").innerHTML = "Kiszámol";
+  document.getElementById("a").style.cursor = "auto";
+  document.getElementById("a").disabled = false;
+  document.getElementById("b").style.cursor = "auto";
+  document.getElementById("b").disabled = false;
+  document.getElementById("c").style.cursor = "auto";
+  document.getElementById("c").disabled = false;
+  reset.style.visibility = "hidden";
+  document.getElementById("area").innerHTML = "";
+});
